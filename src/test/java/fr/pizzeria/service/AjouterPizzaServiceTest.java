1e69130580ba57746b7f30e097d478c1bc786b45
Patrice Allardin <patrice.allardin@free.fr>
2019-05-07 11:22:52 +0200

    tp02v1.6

--- src/test/java/fr/pizzeria/service/AjouterPizzaServiceTest.java
+++ src/test/java/fr/pizzeria/service/AjouterPizzaServiceTest.java
@@ -1,5 +1,6 @@
 package fr.pizzeria.service;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
 
@@ -21,7 +22,7 @@ public class AjouterPizzaServiceTest {
 	public TextFromStandardInputStream systemInMock = emptyStandardInputStream();
 
 	@Test
-	public void testExecuteUC() {
+	public void testExecuteUCVrai() {
 		// Stockage des lignes de scanner à jouer
 		systemInMock.provideLines("ARG", "Argentine", "12.3", "Viande");
 		// Création d'une DAO pour la pizzeria
@@ -33,6 +34,20 @@ public class AjouterPizzaServiceTest {
 		// Vérification que cela s'est bien passé en vérifiant que la pizza
 		// existe
 		assertTrue(pmd.pizzaExists("ARG"));
+	}
+
+	@Test
+	public void testExecuteUCFaux() {
+		// Stockage des lignes de scanner à jouer
+		systemInMock.provideLines("ALB", "Albanaise", "12.3", "Viande");
+		// Création d'une DAO pour la pizzeria
+		IPizzaDao pmd = new PizzaMemDao();
+		// Création de service d'ajout de pizza
+		AjouterPizzaService aps = new AjouterPizzaService();
+		// Exécution de Use Case d'ajout
+		aps.executeUC(new Scanner(System.in), pmd);
+		// Vérification que la pizza non réelle n'est pas ajoutée
+		assertFalse(pmd.pizzaExists("ARG"));
 	}
 
 }
