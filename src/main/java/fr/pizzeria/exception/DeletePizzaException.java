02dec94525be25fed4ec848a23edd840b260112a
Patrice <Patrice@HP-PHOENIX>
2019-05-11 11:03:17 +0200

    1er commit externe d'exception

--- src/main/java/fr/pizzeria/exception/DeletePizzaException.java
+++ src/main/java/fr/pizzeria/exception/DeletePizzaException.java
@@ -1,5 +1,14 @@
 package fr.pizzeria.exception;
 
+/**
+ * Exception de suppression de pizza
+ * @author Patrice
+ *
+ */
 public class DeletePizzaException extends StockageException {
+	
+	public DeletePizzaException () {
+		
+	}
 
 }
